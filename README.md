4623b2622da9542fe0ead7eeff3de8c8eaebd259
Shrish Kukde <kukde.shrish@gmail.com>
2020-12-18 14:08:36 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Fun-With-Views :heart:
+A very simple and fun application to play with view and at the same time this application will help you in understanding the concept of:
+1. Constraint Layout
+2. View Binding
+
+
+---
+#### Pre-requisites
+For this app you should be familiar with:  
+1. Download and run apps in Android Studio.
+2. Kotlin Language.
+
+---
+#### Getting Started
+
+Download the source code and open the app in Android Studio and run on Emulator or physical device.  
+
+---
+##### Copyright Details:
+###### Please note that this application source code is written by referring to *ConstraintLayout using the Layout Editor*
+###### Please refer to thier Copyright License for more details.
---
+++ app/src/main/java/om/funwithviews/MainActivity.kt
@@ -0,0 +1,56 @@
+package om.funwithviews
+
+import android.graphics.Color
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import om.funwithviews.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        setListeners()
+    }
+
+    private fun setListeners() {
+        val clickableViews: List<View> = listOf(
+            binding.boxOneText,
+            binding.boxTwoText,
+            binding.boxThreeText,
+            binding.boxFourText,
+            binding.boxFiveText,
+            binding.constraintLayout,
+            binding.redButton,
+            binding.greenButton,
+            binding.yellowButton
+        )
+        for (item in clickableViews) {
+            item.setOnClickListener { makeColored(it) }
+        }
+    }
+
+    private fun makeColored(view: View) {
+        when (view.id) {
+
+            R.id.box_one_text -> view.setBackgroundColor(Color.BLUE)
+            R.id.box_two_text -> view.setBackgroundColor(Color.MAGENTA)
+
+            R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+            R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
+            R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            R.id.red_button -> binding.boxThreeText.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> binding.boxFourText.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> binding.boxFiveText.setBackgroundResource(R.color.my_green)
+
+            else -> view.setBackgroundColor(Color.TRANSPARENT)
+
+        }
+    }
+}
